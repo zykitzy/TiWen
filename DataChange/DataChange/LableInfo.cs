db31956b205dc24958626ee14d93015d5f4c3135
zhang yi <zykitzlm@126.com>
2018-05-14 00:32:38 +0800

    家里修改的代码
    家里修改的代码

---
+++ DataChange/DataChange/LableInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataChange
+{
+    public class LableInfo : INotifyPropertyChanged
+    {
+        private string lableid;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string LableID
+        {
+            get
+            {
+                return lableid;
+            }
+            set
+            {
+                lableid = value;
+                if (PropertyChanged != null)
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("LableID"));
+            }
+        }
+        private string lablevalue;
+        public string LableValue
+        {
+            get
+            {
+                return lablevalue;
+            }
+            set
+            {
+                lablevalue = value;
+                if (PropertyChanged != null)
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("LableValue"));
+            }
+        }
+        private string convretvalue;
+        public string ConvertValue
+        {
+            get
+            {
+                return convretvalue;
+            }
+            set
+            {
+                convretvalue = value;
+                if (PropertyChanged != null)
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("ConvertValue"));
+            }
+        }
+
+    }
+}
--- DataChange/DataChange/MainWindow.xaml.cs
+++ DataChange/DataChange/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DataChange
 {
@@ -20,13 +23,69 @@ namespace DataChange
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool ismonitor = false;
+        private DispatcherTimer timer;
+        private int timertick = 0;
+        private ObservableCollection<LableInfo> data;
+
+        public ObservableCollection<LableInfo> Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+
+            data = new ObservableCollection<LableInfo>();
+            Data.Add(new LableInfo() { LableID="test", LableValue="1111",ConvertValue="测试" });
+
+            listShow.ItemsSource = Data;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (data.Count > 15)
+            {
+                data.RemoveAt(0);
+            }
+            var info = new LableInfo();
+            info.LableID = timertick.ToString();
+            info.LableValue = $"第{timertick}秒";
+            info.ConvertValue = new Random().Next(timertick).ToString();
+            Data.Add(info);
+            listShow.ItemsSource = Data;
+            timertick++;
+
         }
 
         private void btnMontor_Click(object sender, RoutedEventArgs e)
         {
+            ismonitor = !ismonitor;
+            if (ismonitor)
+            {
+                timer.Start();
+                btnMontor.Content = "取消监听";
+                btnMontor.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                timer.Stop();
+                btnMontor.Content = "开始监听";
+                btnMontor.Foreground = new SolidColorBrush(Colors.Black);
+            }
+
 
         }
 
