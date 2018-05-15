57a5cf447a180b5181309bd42f8eae1f9c60fde2
zhang yi <zykitzlm@126.com>
2018-05-15 18:23:22 +0800

    新增报文
    新增基站IP报文，同步发送，修改配置项

--- DataChange/ConsoleData/Program.cs
+++ DataChange/ConsoleData/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleData
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             var listen = new ListenClass();
             Console.WriteLine("输入EXIT退出");
             listen.startListen();
@@ -26,8 +27,6 @@ namespace ConsoleData
                     break;
                 }
             }
-
-
         }
 
         public class ListenClass
@@ -112,6 +111,7 @@ namespace ConsoleData
                         }
                         //响应昂科数据
                         RecLocalTag(buffer);
+                        Console.ForegroundColor = ConsoleColor.White;
                         //发送卡迪数据
                         SendUseKadi(buffer);
                     }
@@ -131,6 +131,7 @@ namespace ConsoleData
                 if (buffer[16].ToString("X2") == "3F" && (buffer[2].ToString("X2") == "22" || buffer[2].ToString("X2") == "27"))
                 {
                     StringBuilder sb = new StringBuilder();
+                    Console.ForegroundColor = ConsoleColor.Green;
                     sb.Append(DateTime.Now.ToString() + " ：");
                     for (int i = 0; i <= 16; i++)
                     {
@@ -172,68 +173,72 @@ namespace ConsoleData
                 //模拟Kadi 发送
                 if (buffer[2].ToString("X2") == "27")
                 {
-                    int i = 8; //包头位移
-                    byte[] response = new byte[35];
+                    byte[] node = new byte[6];
+
                     //前八位是基站头
                     //获取卡迪基站模拟,必须空格分割
-                    var tagServer = ConfigurationManager.AppSettings["TagServer"].Split(' ');
+                    var tagServer = ConfigurationManager.AppSettings["TagServer"].Split(',')[0].Split(' ');
                     if (tagServer.Count() != 6)
                     {
                         throw new Exception("TagServer 无法模拟，必须是6个字节以空格分隔");
                     }
                     for (int f = 0; f < 6; f++)
                     {
-                        response[f] = (byte)Convert.ToInt32($"0x{tagServer[f]}", 16);
+                        node[f] = (byte)Convert.ToInt32($"0x{tagServer[f]}", 16);
                     }
-                    //后两位是本次报文数据量，默认每次发送一条
-                    response[6] = 0x01;
-                    response[7] = 0x00;
-                    //参数参考 TSS 800，FD固定，Normal 状态为 00
-                    response[0 + i] = 0xFD;
-                    response[1 + i] = 0x00;
-                    // 第三位到第八位是tag，通过config 匹配 昂科标签
-                    var tags = ConfigurationManager.AppSettings["TagClient"].Split(',');
-                    foreach (var tag in tags)
+
+
+                    //配置kadi 体温
+                    var tiwen = GetTiwenTag(buffer);
+                    // 配置基站心跳
+                    var xintiao = ServerXinTiao(node);
+                    //基站IP
+                    var serverip = ServerIP(node);
+
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var no in node)
                     {
-                        //匹配标签，昂科标签转Kadi, | 前为昂科标签，后卫卡迪标签
-                        if (buffer[7] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[0]), 16)
-                            && buffer[8] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[1]), 16)
-                                  && buffer[9] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[2]), 16)
-                                        && buffer[10] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[3]), 16)
-                            )
-                        {
-                            for (int f = 2; f <= 7; f++)
-                            {
-                                response[f + i] = (byte)Convert.ToInt32($"0x{tag.Split('|')[1].Split(' ')[f - 2]}", 16);
-                            }
-                        }
-                        break;
+                        sb.Append(no.ToString("X2"));
+                        sb.Append(" ");
                     }
-                    //以下 五位未知，可能影响时间
-                    response[8 + i] = 0x00;
-                    response[9 + i] = 0x23;
-                    response[10 + i] = 0x10;
-                    response[11 + i] = 0x00;
-                    response[12 + i] = 0xFB;
-                    //以下4位温度，小数点第三位默认0
-                    response[13 + i] = buffer[12];
-                    int t10 = buffer[13];
-                    response[14 + i] = (byte)(t10 / 10);
-                    response[15 + i] = (byte)(t10 % 10);
-                    response[16 + i] = 0x00;
-                    //Kadi 预留位
-                    response[17 + i] = 0x00;
-                    response[18 + i] = 0x00;
-                    response[19 + i] = 0x05;
-                    response[20 + i] = 0x00;
-                    // 时间参数
-                    response[21 + i] = (byte)DateTime.Now.Day;
-                    response[22 + i] = (byte)DateTime.Now.Month;
-                    //年份 20XX
-                    response[23 + i] = (byte)(DateTime.Now.Year - 2000);
-                    response[24 + i] = (byte)DateTime.Now.Hour;
-                    response[25 + i] = (byte)DateTime.Now.Minute;
-                    response[26 + i] = (byte)DateTime.Now.Second;
+                    Console.WriteLine("基站 : " + sb);
+
+                    byte[] response = new byte[node.Length + 2 + tiwen.Length 
+                                                    + xintiao.Length + serverip.Length];
+                    Buffer.BlockCopy(node, 0, response, 0, 6);
+                    //后两位是本次报文数据量
+                    //1 体温 2 心跳 3 基站IP
+                    response[6] = 0x03;
+                    response[7] = 0x00;
+
+                    Buffer.BlockCopy(tiwen, 0, response, 8, 27);
+                    sb.Clear();
+                    foreach (var no in tiwen)
+                    {
+                        sb.Append(no.ToString("X2"));
+                        sb.Append(" ");
+                    }
+                    Console.WriteLine("体温 : " + sb);
+
+                    Buffer.BlockCopy(xintiao, 0, response, 35, 27);
+                    sb.Clear();
+
+                    foreach (var no in xintiao)
+                    {
+                        sb.Append(no.ToString("X2"));
+                        sb.Append(" ");
+                    }
+                    Console.WriteLine("心跳 : " + sb);
+
+                    Buffer.BlockCopy(serverip, 0, response, 62, 27);
+                    sb.Clear();
+                    foreach (var no in serverip)
+                    {
+                        sb.Append(no.ToString("X2"));
+                        sb.Append(" ");
+                    }
+                    Console.WriteLine("基站IP : " + sb);
+
                     //配置发送体温APP
                     var ipaddress = ConfigurationManager.AppSettings["NurseServer"];
                     IPAddress ip = IPAddress.Parse(ipaddress.Split(',')[0]);
@@ -249,34 +254,166 @@ namespace ConsoleData
                     {
                         socket.Connect(ipEnd);
                         Console.WriteLine("体温 : "
-                            + response[13 + i] + "." //整数
-                            + response[14 + i] //小数第一位
-                            + response[15 + i]); //小数第二位
-                        Console.WriteLine($"时间 : {response[23 + i]} 年" +
-                            $"{response[22 + i]} 月" +
-                            $"{response[21 + i]} 日" +
-                            $"{response[24 + i]} :" +
-                            $"{response[25 + i]} :" +
-                            $"{response[26 + i]} :");
-                        socket.Send(response, 35, SocketFlags.None);
+                            + response[21] + "." //整数
+                            + response[22] //小数第一位
+                            + response[23]); //小数第二位
+                        Console.WriteLine($"时间 : {response[31]} 年" +
+                            $"{response[30]} 月" +
+                            $"{response[29]} 日" +
+                            $"{response[32]} :" +
+                            $"{response[33]} :" +
+                            $"{response[34]}");
 
-                        Console.WriteLine("disconnect from server");
+                        socket.Send(response, response.Length, SocketFlags.None);
+
+                        Console.WriteLine($"Send {response.Length} Byte To {ipaddress} Success!");
                         socket.Shutdown(SocketShutdown.Both);
                         socket.Close();
+                        Console.WriteLine($"Disconnect Server");
                     }
                     //异常处理
                     catch (SocketException e)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Fail to connect server");
                         Console.WriteLine(e.ToString());
                         return;
                     }
                 }
             }
-            string FormatByte(byte btr)
+
+            /// <summary>
+            /// 配置体温标签
+            /// </summary>
+            /// <param name="buffer"></param>
+            /// <returns></returns>
+            private byte[] GetTiwenTag(byte[] buffer)
             {
-                return btr.ToString("X2");
+                var response = new byte[27];
+                //参数参考 TSS 800，FD固定，Normal 状态为 00
+                response[0] = 0xFD;
+                response[1] = 0x00;
+
+
+                // 第三位到第八位是tag，通过config 匹配 昂科标签
+                var tags = ConfigurationManager.AppSettings["TagClient"].Replace("\r\n", "").Split(',');
+                foreach (var tag in tags)
+                {
+                    //匹配标签，昂科标签转Kadi, | 前为昂科标签，后卫卡迪标签
+                    if (buffer[7] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[0]), 16)
+                        && buffer[8] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[1]), 16)
+                        && buffer[9] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[2]), 16)
+                        && buffer[10] == (byte)Convert.ToInt32((tag.Split('|')[0].Split(' ')[3]), 16)
+                        )
+                    {
+                        for (int f = 2; f <= 7; f++)
+                        {
+                            response[f] = (byte)Convert.ToInt32($"0x{tag.Split('|')[1].Split(' ')[f - 2]}", 16);
+                        }
+                        break;
+                    }
+                    //    continue;
+                }
+                //以下 五位未知，可能影响
+                response[8] = 0x00;
+                response[9] = 0x23;
+                response[10] = 0x10;
+                response[11] = 0x00;
+                response[12] = 0xFB;
+                //以下4位温度，小数点第三位默认0
+                response[13] = buffer[12];
+                int t10 = buffer[13];
+                response[14] = (byte)(t10 / 10);
+                response[15] = (byte)(t10 % 10);
+                response[16] = 0x00;
+                //Kadi 预留位
+                response[17] = 0x00;
+                response[18] = 0x00;
+                response[19] = 0x05;
+                response[20] = 0x00;
+                // 时间参数
+                response[21] = (byte)DateTime.Now.Day;
+                response[22] = (byte)DateTime.Now.Month;
+                //年份 20XX
+                response[23] = (byte)(DateTime.Now.Year - 2000);
+                response[24] = (byte)DateTime.Now.Hour;
+                response[25] = (byte)DateTime.Now.Minute;
+                response[26] = (byte)DateTime.Now.Second;
+
+                return response;
             }
+
+            /// <summary>
+            /// 心跳报文
+            /// </summary>
+            /// <param name="server">基站6字节编码</param>
+            /// <returns></returns>
+            private byte[] ServerXinTiao(byte[] server)
+            {
+                var node = new byte[27];
+                //心跳前两位固定
+                node[0] = 0x23;
+                node[1] = 0x00;
+                //六位基站
+                Buffer.BlockCopy(server, 0, node, 2, 6);
+                // 8-19 固定编码
+                node[8] = 0x00;
+                node[9] = 0x00;
+                node[10] = 0x00;
+                node[11] = 0x00;
+                node[12] = 0x0A;
+                node[13] = 0x0A;
+                node[14] = 0x05;
+                node[15] = 0x1B;
+                node[16] = 0x00;
+                node[17] = 0x00;
+                node[18] = 0x00;
+                node[19] = 0x00;
+                //20 位 net 固定 00 Normal
+                node[20] = 0x00;
+                //6位时间
+                // 时间参数
+                node[21] = (byte)DateTime.Now.Day;
+                node[22] = (byte)DateTime.Now.Month;
+                //年份 20XX
+                node[23] = (byte)(DateTime.Now.Year - 2000);
+                node[24] = (byte)DateTime.Now.Hour;
+                node[25] = (byte)DateTime.Now.Minute;
+                node[26] = (byte)DateTime.Now.Second;
+                return node;
+            }
+
+            //发送IP数据
+            private byte[] ServerIP(byte[] node)
+            {
+                var tagServer = ConfigurationManager.AppSettings["TagServer"].Split(',')[1].Split('.');
+                var serverip = new byte[27];
+                //IP 第一位固定
+                serverip[0] = 0xFE;
+                for (int i = 0; i < 4; i++)
+                {
+                    serverip[i + 1] = (byte)Convert.ToInt32(tagServer[i]);
+                }
+                Buffer.BlockCopy(node, 0, serverip, 5, 6);
+                //11到20位固定为0
+                for (int i = 11; i < 21; i++)
+                {
+                    serverip[i] = 0x00;
+                }
+                //6位时间
+                // 时间参数
+                serverip[21] = (byte)DateTime.Now.Day;
+                serverip[22] = (byte)DateTime.Now.Month;
+                //年份 20XX
+                serverip[23] = (byte)(DateTime.Now.Year - 2000);
+                serverip[24] = (byte)DateTime.Now.Hour;
+                serverip[25] = (byte)DateTime.Now.Minute;
+                serverip[26] = (byte)DateTime.Now.Second;
+
+                return serverip;
+            }
+
+
 
         }
     }
